306a2ae72f744aabfc8f165b5cb58585d855e2a5
Praba <prama1303@gmail.com>
2022-09-14 20:34:20 +0200

    Keyboard interactions testing test

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -9,6 +9,10 @@ describe('Counter', () => {
     expect(countElement).toBeInTheDocument();
     const incrementButton = screen.getByRole('button', { name: 'Increment' });
     expect(incrementButton).toBeInTheDocument();
+    const amountInput = screen.getByRole('spinbutton');
+    expect(amountInput).toBeInTheDocument();
+    const setButton = screen.getByRole('button', { name: 'Set' });
+    expect(setButton).toBeInTheDocument();
   });
 
   test('renders a count of 0', () => {
@@ -34,5 +38,30 @@ describe('Counter', () => {
     await user.click(incrementButton); //wichtig
     const countElement = screen.getByRole('heading');
     expect(countElement).toHaveTextContent('2');
+  });
+
+  test('rendres a count of 10 after clicking the set button', async () => {
+    user.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole('spinbutton');
+    await user.type(amountInput, '10');
+    expect(amountInput).toHaveValue(10);
+    const setButton = screen.getByRole('button', { name: 'Set' });
+    await user.click(setButton);
+    const countElement = screen.getByRole('heading'); //find heading element
+    expect(countElement).toHaveTextContent('10');
+  });
+  test('elements are focused in the right order', async () => {
+    user.setup();
+    render(<Counter />);
+    const amountInput = screen.getByRole('spinbutton');
+    const setButton = screen.getByRole('button', { name: 'Set' });
+    const incrementButton = screen.getByRole('button', { name: 'Increment' });
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+    await user.tab();
+    expect(setButton).toHaveFocus();
   });
 });
