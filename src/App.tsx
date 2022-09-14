a962f447e9f19573348d1020ce3fecbd3b68e7d2
Praba <prama1303@gmail.com>
2022-09-14 23:31:02 +0200

    AppProviders wrapp rendering test

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import { Application } from './components/application/Application';
 import { Counter } from './components/counter/Counter';
+import { MuiMode } from './components/mui/MuiMode';
 import { Skills } from './components/skills/Skills';
+import { AppProviders } from './providers/AppProviders';
 
 function App() {
   return (
-    <div className='App'>
-      {/*<Application />*/}
-      {/*<Skills skills={['HTML', 'CSS']} />*/}
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className='App'>
+        {/*<Application />*/}
+        {/*<Skills skills={['HTML', 'CSS']} />*/}
+        {/*<Counter />*/}
+
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
---
+++ src/components/mui/MuiMode.test.tsx
@@ -0,0 +1,19 @@
+//import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../test-utils';
+import { AppProviders } from '../../providers/AppProviders';
+import { MuiMode } from './MuiMode';
+
+describe('MuiMode', () => {
+  test('renders text correctly', () => {
+    //render(<MuiMode />); // so fehler, da nicht provider nicht eingeschlossen ist
+    {
+      /*  render(<MuiMode />, {
+      wrapper: AppProviders
+    });
+    */
+    }
+    render(<MuiMode />); // so gehts with test-utils
+    const headingElement = screen.getByRole('heading');
+    expect(headingElement).toHaveTextContent('dark mode');
+  });
+});
