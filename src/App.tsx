08d01a347f5b97b3b5c0d3044520661ae69f1dbf
Praba <prama1303@gmail.com>
2022-09-14 09:23:01 +0200

    Counter ex. with pointer interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Application } from './components/application/Application';
+import { Counter } from './components/counter/Counter';
 import { Skills } from './components/skills/Skills';
 
 function App() {
   return (
     <div className='App'>
-      <Application />
-      <Skills skills={['HTML', 'CSS']} />
+      {/*<Application />*/}
+      {/*<Skills skills={['HTML', 'CSS']} />*/}
+      <Counter />
     </div>
   );
 }
